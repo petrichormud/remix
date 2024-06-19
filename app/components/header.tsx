5ef44fe313c67df5804f32eb94cd03d039a5707e
Alec DuBois <after.alec@gmail.com>
2024-06-19 11:09:36 -0700

    Switch To New York Style ShadCN

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,8 +6,10 @@ type HeaderProps = {
 
 export function Header({ pid }: HeaderProps) {
   return (
-    <header className="flex w-full items-center justify-end py-2 px-6 border-b">
-      <Navigation pid={pid} />
+    <header className="flex w-full items-center justify-center py-2 px-6 border-b">
+      <section className="flex w-full max-w-screen-2xl items-center justify-end">
+        <Navigation pid={pid} />
+      </section>
     </header>
   );
 }
