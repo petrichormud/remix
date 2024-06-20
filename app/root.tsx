ec8b24684bb3126017d12c6e970444e530d7d652
Alec DuBois <after.alec@gmail.com>
2024-06-20 16:37:22 -0700

    Experiment With Provided App Instead Of Layout
    Removes the Layout pattern in favor of a Document component and a
    provided App, in preparation for supporting a theme.

--- app/root.tsx
+++ app/root.tsx
@@ -9,12 +9,15 @@ import {
 
 import { ThemeContext } from "~/context/theme";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+function Document({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,viewport-fit=cover"
+        />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <Meta />
         <Links />
@@ -28,11 +31,38 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+// export function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en" className="dark">
+//       <head>
+//         <meta charSet="utf-8" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body>
+//         {children}
+//         <ScrollRestoration />
+//         <Scripts />
+//       </body>
+//     </html>
+//   );
+// }
+
+function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export default function AppWithProviders() {
   const [theme, setTheme] = useState<"light" | "dark">("light");
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <Outlet />
+      <App />
     </ThemeContext.Provider>
   );
 }
