1ee1b00c52750d29a772f2d799274e708a72a67f
Alec DuBois <after.alec@gmail.com>
2024-06-25 09:28:46 -0700

    Remove Unused Header Components on Data Table

--- app/components/players/data-table.tsx
+++ app/components/players/data-table.tsx
@@ -9,20 +9,10 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
 
 import { DataTablePagination } from "~/components/ui/data-table-pagination";
 import { DataTable } from "~/components/ui/data-table";
-import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -66,28 +56,6 @@ export function PlayersDataTable<TData, TValue>({
           }
           className="max-w-sm"
         />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="ghost"
-              className="h-8 w-8 p-0"
-              disabled={!table.getFilteredSelectedRowModel().rows.length}
-            >
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Grant Permissions</DropdownMenuItem>
-            <DropdownMenuItem>Revoke All Permissions</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <div className="flex-1 flex place-self-end justify-end items-end text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
       </div>
       <DataTable table={table} columns={columns} />
       <div className="pt-2">
